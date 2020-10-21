de78d073aefb65e6a992696d4ca86ac434462edd
Supamaero <muwede6@gmail.com>
2020-10-21 15:11:22 +0100

    new update

---
+++ script.js
@@ -0,0 +1,15 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function mySortFunction() {
+    document.getElementById("mySortDropdown").classList.toggle("show");
+  }
+
+
+  function myFilterFunction() {
+    document.getElementById("myFilterDropdown").classList.toggle("shows");
+  }
+
+
+
+  
+
